63c1bcfbcbfec4c729d5141dd8f87ac453392c2e
marcin wiktor <witar1@gmail.com>
2019-06-29 14:38:31 +0200

    Bubble Sort

---
+++ src/main/java/Sortowanie/SellectionSort.java
@@ -0,0 +1,18 @@
+package Sortowanie;
+
+public class SellectionSort {
+    public static void selectionSort(int[] array){
+        for (int i = 0; i < array.length - 1; i++)
+        {
+            int index = i;
+            for (int j = i + 1; j < array.length; j++){
+                if (array[j] < array[index]){
+                    index = j;
+                }
+            }
+            int smallerNumber = array[index];
+            array[index] = array[i];
+            array[i] = smallerNumber;
+        }
+    }
+}
