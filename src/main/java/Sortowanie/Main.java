5761dce0270996306a0ff189d986f248e3f3ffe7
marcin wiktor <witar1@gmail.com>
2019-06-30 09:45:36 +0200

    Wszystkie sortowania razem

--- src/main/java/Sortowanie/Main.java
+++ src/main/java/Sortowanie/Main.java
@@ -4,17 +4,15 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[] arrayToSort = {3, 4, 8, 1, 5};
+        int[] arrayToSort = {3, 4, 8, 1, 5, 11, 25, 9, 8, 45};
         System.out.println("Count Sort");
         CountSort.countSort(arrayToSort, 101);
         System.out.println(Arrays.toString(arrayToSort));
         System.out.println("Bubble Sort");
         BubbleSort.bubbleSort(arrayToSort);
         System.out.println(Arrays.toString(arrayToSort));
-        SellectionSort.selectionSort(arrayToSort);
         System.out.println("Sellection Sort");
-        for(int i:arrayToSort){
-           System.out.print("[" + i + "]" + ",");
-        }
+        SellectionSort.selectionSort(arrayToSort);
+        System.out.println(Arrays.toString(arrayToSort));
     }
 }
